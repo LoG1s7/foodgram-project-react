037a88c67db295e337e78fee37d569485231eb61
Alexander Kolesnikov <alexkolesnikov.git@gmail.com>
2023-05-15 23:30:50 +0300

    fixes infra

--- backend/api/views.py
+++ backend/api/views.py
@@ -20,8 +20,7 @@ from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT
 from users.models import User
